6a1fff44d5e3f67e6d81e872453ec238f4316ec8
Ming Li <46766796+danwslightinc@users.noreply.github.com>
2023-05-20 19:33:46 -0400

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dli.redis.springredis.repo;
-
-import com.dli.redis.springredis.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-}
